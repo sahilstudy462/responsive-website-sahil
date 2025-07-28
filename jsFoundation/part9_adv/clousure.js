716f1088d2367b60d0b0a2cbfb95d8a84f449099
sahilstudy462 <sahilstudy462@gmail.com>
2025-07-28 23:07:48 +0530

    clousure.js

--- jsFoundation/part9_adv/clousure.js
+++ jsFoundation/part9_adv/clousure.js
@@ -1,3 +1,6 @@
+/*outer() returns an anonymous inner function that:
+Increments the counter variable
+Returns the new value of counter */
 function outer() {
   let counter = 4;
   return function () {
@@ -6,7 +9,17 @@ function outer() {
   };
 }
 
+
+/* outer() is called, and its returned function is assigned to increment
+The magic happens here: even though outer() has finished executing, the inner function maintains access to counter through a closure */
 let increment = outer();
 console.log(increment());
 console.log(increment());
 console.log(increment());
+/* Accesses the counter variable from the closure
+Increments it
+Returns the new value */
+
+// Closure: The inner function "closes over" (remembers) the counter variable from its parent scope, even after the parent function has completed execution.
+
+//Private State: The counter variable is effectively private - it can only be modified through the increment function.
