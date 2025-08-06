7024b991b73661d88a4a97c52d2ee53b926f67f9
sahilstudy462 <sahilstudy462@gmail.com>
2025-08-06 18:23:16 +0530

    mathOperationsM.js

--- jsFoundation/part9_adv/mathOperationsM.js
+++ jsFoundation/part9_adv/mathOperationsM.js
@@ -1,3 +1,8 @@
+//ES6 modules in JavaScript
+
+//Named Exports:
+//add and subtract functions are exported as named exports using the export keyword
+//These must be imported using the exact same name in curly braces
 export function add(a, b) {
   return a + b;
 }
@@ -6,6 +11,11 @@ export function subtract(a, b) {
   return a - b;
 }
 
+
+//Default Export:
+//multiply is exported as the default export using export default
+//A module can have only one default export
+//When importing, you can choose any name for the default import
 export default function multiply(a, b) {
   return a * b;
 }
