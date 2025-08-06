d0206ac498177d18233be5f7e79f055722b15a8c
sahilstudy462 <sahilstudy462@gmail.com>
2025-08-06 18:24:13 +0530

    mathOperationsC.js

--- jsFoundation/part9_adv/mathOperationsC.js
+++ jsFoundation/part9_adv/mathOperationsC.js
@@ -1,5 +1,8 @@
-//named export
+//CommonJS module system
 
+//named export
+//Three functions are defined normally (add, subtract, multiply)
+//At this point, they're only available within the module file
 function add(a, b) {
   return a + b;
 }
@@ -12,6 +15,10 @@ function multiply(a, b) {
   return a * b;
 }
 
+
+//module.exports is used to expose these functions to other files
+//We're exporting an object containing all three functions
+//This is similar to named exports in ES6 modules, but with different syntax
 module.exports = {
   add,
   subtract,
