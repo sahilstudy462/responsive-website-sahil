d73d0df59738871fe068208ef190a1dcedf66e23
sahilstudy462 <sahilstudy462@gmail.com>
2025-09-18 17:54:19 +0530

    Update changes.js

--- jsFoundation/part1/changes.js
+++ jsFoundation/part1/changes.js
@@ -1,11 +1,14 @@
-let gameName = "spiderman";
+// Changes in let and const
+// Example 1
+let gameName = "spiderman"; // Declaration and Initialization
+gameName = "batman"; // Re-assignment
+console.log(gameName); // batman
 
-gameName = "batman";
 
-console.log(gameName);
 
-const username = "hiteshdotcom";
-
-username = "hitesh";
-
-console.log(username);
+// Example 2
+// const gameName = "spiderman"; // Declaration and Initialization
+const username = "hiteshdotcom"; // Declaration and Initialization
+username = "hitesh"; // Re-assignment
+console.log(username); // Error: Assignment to constant variable. // const variable cannot be re-assigned
+// Note: const variable must be initialized during declaration.
