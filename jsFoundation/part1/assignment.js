bed97163416198b9b5695bb4344310a9f89f5504
sahilstudy462 <sahilstudy462@gmail.com>
2025-09-18 17:53:48 +0530

    Update assignment.js

--- jsFoundation/part1/assignment.js
+++ jsFoundation/part1/assignment.js
@@ -1,8 +1,11 @@
+// Assignment Operators
+// =, +=, -=, *=, /=, %=
+// Example 1
 let score = 10 + 11;
+console.log(score);
 
+// Example 2
 let num1 = 10;
-
-// num1 = num1 + 5
+// num1 = num1 / 5
 num1 /= 5;
-
 console.log(num1);
