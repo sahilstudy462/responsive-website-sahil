6f99730d15399afb589bd581dcbd95f09a79046b
sahilstudy462 <sahilstudy462@gmail.com>
2025-09-19 10:54:06 +0530

    Update assignment.js

--- jsFoundation/part1/assignment.js
+++ jsFoundation/part1/assignment.js
@@ -2,10 +2,32 @@
 // =, +=, -=, *=, /=, %=
 // Example 1
 let score = 10 + 11;
-console.log(score);
+console.log(score); // output: 21
+
 
 // Example 2
 let num1 = 10;
 // num1 = num1 / 5
 num1 /= 5;
-console.log(num1);
+console.log(num1); // output: 2
+
+
+// Example 3
+let num2 = 10;
+// num2 = num2 * 5
+num2 *= 5;
+console.log(num2); // output: 50
+
+
+// Example 4
+let num3 = 10;
+// num3 = num3 + 5
+num3 += 5;
+console.log(num3); // output: 15
+
+
+// Example 5
+let num4 = 10;
+// num4 = num4 - 5
+num4 -= 5;
+console.log(num4); // output: 5
