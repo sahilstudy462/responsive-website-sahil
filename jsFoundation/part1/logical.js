8f2c5f6d5a9f7145e07d6ad973201bdd2e702189
sahilstudy462 <sahilstudy462@gmail.com>
2025-09-19 11:24:06 +0530

    Update logical.js

--- jsFoundation/part1/logical.js
+++ jsFoundation/part1/logical.js
@@ -4,10 +4,46 @@
 
 let isLoggedin = true;
 let ispaid = true;
-
-// console.log(isLoggedin && ispaid);
-
-let isEmailuser = true;
 let isGoogleuser = false;
 
-console.log(isEmailuser || isGoogleuser);
+
+console.log(isLoggedin || ispaid); //true
+console.log(isLoggedin && ispaid); //true
+console.log(isLoggedin || isGoogleuser); //true
+console.log(isLoggedin && isGoogleuser); //false
+console.log(!isLoggedin); //false
+console.log(!isGoogleuser); //true
+console.log((isLoggedin && ispaid) || isGoogleuser); //true
+console.log((isLoggedin || ispaid) && isGoogleuser); //false
+
+// Example 2
+let highscore = 100;
+let userScore = 90;
+
+console.log(userScore > highscore); //false
+console.log(userScore < highscore); //true
+console.log(userScore >= highscore); //false
+console.log(userScore <= highscore); //true
+console.log(userScore == highscore); //false
+console.log(userScore != highscore); //true
+
+// Example 3
+let username = "hitesh";
+let password = "12345";
+
+console.log(username === "hitesh" && password === "12345"); //true
+console.log(username === "hitesh" || password === "1234"); //true
+console.log(!(username === "hitesh")); //false
+console.log(!(password === "12345")); //false
+console.log(username === "hitesh" && !(password === "12345")); //false
+console.log(username === "hitesh" || !(password === "12345")); //true
+
+
+// Example 4
+let user = "admin";
+let pass = "admin123";
+
+console.log(user === "admin" && pass === "admin123"); //true
+console.log(user === "admin" || pass === "admin"); //true
+console.log(!(user === "admin")); //false
+console.log(!(pass === "admin123")); //false
