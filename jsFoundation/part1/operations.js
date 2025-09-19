4495b974d3ebf230d72fbcfae74a220bf09d65b8
sahilstudy462 <sahilstudy462@gmail.com>
2025-09-19 11:43:46 +0530

    Update operations.js

--- jsFoundation/part1/operations.js
+++ jsFoundation/part1/operations.js
@@ -15,23 +15,34 @@ console.log(mult) // output will be : 15
 let divi = 8 / 2;
 console.log(divi) // output will be : 4
 
+
+//% ko modulus operator kehte hain.
+//Yeh operator division ke baad bacha hua remainder (shesh) return karta hai.
+//9 ko 2 se divide karo, aur jo remainder bache, woh return karo.
+//Divide 9 by 2:
+//2 * 4 = 8
+//9 - 8 = 1 (yeh remainder hai)
 let reminder = 9 % 2;
 console.log(reminder) // output will be : 1
 
+
+//Exponentiation operator (**)
+//Yeh operator base ko exponent ke power se multiply karta hai.
+//2 ** 3 means 2 * 2 * 2 = 8
 let expo = 2 ** 3;
 console.log(expo) // output will be : 8
 
-let myscore = 110;
-myscore++;
+// Increment operator
+let myscore = 110; // yeh 110 hai
+myscore++; // ab yeh 111 ho gaya hai
 console.log(myscore) // output will be : 111
 
-let credits = 56;
-credits--;
+let credits = 56; // yeh 56 hai
+credits--; // ab yeh 55 ho gaya hai
 console.log(credits) // output will be : 55
 
 
 // Comparion operation
-
 let num1 = 3;
 let num2 = 3;
 let num3 = 6;
