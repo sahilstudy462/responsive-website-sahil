392ce85f24dcfc485003501d2a1bc97533e25f2d
sahilstudy462 <sahilstudy462@gmail.com>
2025-09-19 12:48:17 +0530

    Update printing.js

--- jsFoundation/part1/printing.js
+++ jsFoundation/part1/printing.js
@@ -1,7 +1,24 @@
-// console.log("Hello");
-// console.log("chai");
-// process.stdout.write("chai");
-// process.stdout.write("chai");
+console.log("Hello");
+console.log("chai"); 
 
-// console.table({ city: "Jaipur" });
-console.warn({ city: "Jaipur" });
+//process.stdout.write() Node.js ka method hai jo output ko same line par print karta hai, bina newline (\n) ke.
+process.stdout.write("chai"); 
+process.stdout.write("chai");
+//Dono "chai" ek hi line me chipak kar print honge.)
+
+/*
+
+Difference with console.log:
+
+console.log() har print ke baad naya line (newline) add karta hai.
+process.stdout.write() sirf diya hua text print karta hai, bina newline ke.
+
+
+Use:
+Jab aapko output ek hi line me ya custom formatting chahiye ho, tab process.stdout.write() use karte hain.
+
+*/
+
+console.table({ city: "Jaipur" }); //Yeh ek object ko table format me print karta hai.
+
+console.warn({ city: "Jaipur" }); //Yeh ek warning message print karta hai, jo console me alag color ya style me dikh sakta hai.
